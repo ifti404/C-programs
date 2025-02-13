d6d5bfadc989b2f456a9fc352c1d685b52986281
Md Ifti <63661869+ifti404@users.noreply.github.com>
2025-02-13 05:33:08 -0800

    Add files via upload

---
+++ Simple Calculator/calculator.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int op, n1, n2, sum;
+    cout << "Welcome to Calculator program!\n";
+    cout << " Select what do you want to do: 1) Addition 2)Substraction 3)Multiplication 4) Division 5) Modulus \n";
+    cin >> op;
+    cout << "Great. Please Choose First Number: \n";
+    cin >> n1;
+    cout << "Now Choose Second Number";
+    cin >> n2;
+
+    switch (op)
+    {
+    case 1:
+        sum = n1 + n2;
+        cout << n1 << "+" << n2 << "=" << sum;
+        break;
+    case 2:
+        sum = n1 - n2;
+        cout << n1 << "-" << n2 << "=" << sum;
+        break;
+    case 3:
+        sum = n1 * n2;
+        cout << n1 << "x" << n2 << "=" << sum;
+        break;
+    case 4:
+        sum = n1 / n2;
+        cout << n1 << "/" << n2 << "=" << sum;
+        break;
+    case 5:
+        sum = n1 % n2;
+        cout << n1 << "%" << n2 << "=" << sum;
+        break;
+    }
+
+    return 0;
+}
